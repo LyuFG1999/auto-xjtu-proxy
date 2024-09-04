bd3e9fbaf4f3ae3acac305dfe41ae72a4c63d6f9
LyuFG1999 <lvfg1999@126.com>
2024-09-04 09:03:04 +0000

    Auto Login for XJTU WebVPN 油猴脚本，自动登录XJTU WEBVPN（需要账号和密码），适用网址：https://webvpn.xjtu.edu.cn/http*/*/openplatform/login.html; https://org.xjtu.edu.cn/*/login.html. 使用时需要把脚本的username和password变量替换成学号和密码。
    Signed-off-by: LyuFG1999 <lvfg1999@126.com>

---
+++ JavaScript/Auto Login for XJTU WebVPN-1.2.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Auto Login for XJTU WebVPN
+// @namespace    https://webvpn.xjtu.edu.cn/
+// @version      1.2
+// @description  Automatically fills username and password and logs in on specific XJTU WebVPN pages
+// @author       LyuFG
+// @match        https://webvpn.xjtu.edu.cn/http*/*/openplatform/login.html
+// @match        https://org.xjtu.edu.cn/*/login.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Define your username and password here
+    var username = "412XXX0000";
+    var password = "password";
+
+    // Wait for the DOM to be fully loaded
+    window.addEventListener('load', function() {
+        // Find the username and password input fields
+        var usernameField = document.querySelector("input[name='username']");
+        var passwordField = document.querySelector("input[name='pwd']");
+
+        // Check if both fields exist
+        if (usernameField && passwordField) {
+            // Fill in the username and password
+            usernameField.value = username;
+            passwordField.value = password;
+
+            // Find and click the login button
+            var loginButton = document.querySelector("div#account_login");
+            if (loginButton) {
+                loginButton.click();
+            }
+        }
+    });
+})();
